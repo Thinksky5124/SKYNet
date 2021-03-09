fa2b1b662792cda21f408ef392eb1739033705cb
Thinksky5124 <Thinksky5124@outlook.com>
2021-03-09 22:41:01 +0800

    wenjian

---
+++ option/include/tensor.h
@@ -0,0 +1,8 @@
+/*
+ * @Author: Thyssen Wen
+ * @Date: 2021-03-09 21:41:25
+ * @LastEditors: Thyssen Wen
+ * @LastEditTime: 2021-03-09 21:41:25
+ * @Description: file content
+ * @FilePath: /SKYNet/option/include/tensor.h
+ */
---
+++ option/source/tensor.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: Thyssen Wen
+ * @Date: 2021-03-09 21:41:11
+ * @LastEditors: Thyssen Wen
+ * @LastEditTime: 2021-03-09 21:41:11
+ * @Description: file content
+ * @FilePath: /SKYNet/option/source/tensor.cpp
+ */
